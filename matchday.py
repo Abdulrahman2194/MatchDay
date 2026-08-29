7dce4ea2e9ce25ac0666502f0babbd2bd28e9650
Abdulrahman <abdobaher345@gmail.com>
2026-08-29 23:29:20 +0300

    detect win/draw/loss from score

--- matchday.py
+++ matchday.py
@@ -3,4 +3,14 @@ import sys
 
 Match = sys.argv[2]
 score = sys.argv[3]
-print(f"Match : {Match} |  Score : {score}")
+goals=score.split("-")
+goals_for = int(goals[0])
+goals_against = int(goals[1])
+if goals_for > goals_against:
+    result = "Win"
+elif goals_for == goals_against:
+    result = "Draw"
+else:
+    result = "Lose"
+    
+print(f"Match : {Match} |  Score : {score} | Result : {result}")
